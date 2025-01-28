5a89562b8bc4cf9786b3c8f39b57da0413343b93
sichi46 <willsonmulenga7@gmail.com>
2025-01-28 22:18:34 +0200

    Downloaded and configured swagger ui

--- obj/Debug/net9.0/ToDoApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/ToDoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7be2d83984cd6ce7450652a49f9dbbd7f46b734")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee2ee4c72986693d0aa52f34cb03126137ab357f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
