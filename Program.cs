fd39b8b427ea1626e4d0762ccbe6d60e25fe899f
sichi46 <willsonmulenga7@gmail.com>
2025-01-28 22:23:34 +0200

    Downloaded and configured swagger ui

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-using System.Runtime.CompilerServices;
 using Microsoft.EntityFrameworkCore;
-
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
@@ -14,7 +12,6 @@ builder.Services.AddOpenApiDocument(config =>
     config.Version = "v1";
 });
 var app = builder.Build();
-
 if (app.Environment.IsDevelopment())
 {
     app.UseOpenApi();
@@ -27,42 +24,41 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-// Create the routes 
-
-app.MapGet("/todoitems", async(TodoDb db) =>
+app.MapGet("/todoitems", async (TodoDb db) =>
     await db.Todos.ToListAsync());
 
-app.MapGet("/todoitems/complete", async(TodoDb db)=>
-    await db.Todos.Where( t => t.IsComplete).ToListAsync());    
+app.MapGet("/todoitems/complete", async (TodoDb db) =>
+    await db.Todos.Where(t => t.IsComplete).ToListAsync());
 
-app.MapGet("/todoitems/{id}", async(TodoDb db, int id) => 
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
     await db.Todos.FindAsync(id)
-        is Todo todo 
-        ? Results.Ok(todo) 
-        : Results.NotFound());
-    
-app.MapPost("/todoitems", async(Todo todo, TodoDb db) =>{
+        is Todo todo
+            ? Results.Ok(todo)
+            : Results.NotFound());
+
+app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+{
     db.Todos.Add(todo);
     await db.SaveChangesAsync();
+
     return Results.Created($"/todoitems/{todo.Id}", todo);
 });
 
+app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
 
-app.MapPut("/todoitems/{id}", async(int id, Todo inputTodo, TodoDb db)=>{
-     
-     var todo = await db.Todos.FindAsync(id);
-     if (todo is null) return Results.NotFound();
+    if (todo is null) return Results.NotFound();
 
-     todo.Name = inputTodo.Name;
-     todo.IsComplete = inputTodo.IsComplete;
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
 
-     await db.SaveChangesAsync();
+    await db.SaveChangesAsync();
 
-     return Results.NoContent();
-
+    return Results.NoContent();
 });
 
-app.MapDelete("/todoitems/{id}", async (int id, TodoDb db)=>
+app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
 {
     if (await db.Todos.FindAsync(id) is Todo todo)
     {
@@ -70,12 +66,8 @@ app.MapDelete("/todoitems/{id}", async (int id, TodoDb db)=>
         await db.SaveChangesAsync();
         return Results.NoContent();
     }
-    else
-    {
-        return Results.NotFound();
-    }
+
+    return Results.NotFound();
 });
 
-
 app.Run();
-    
--- obj/Debug/net9.0/ToDoApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/ToDoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee2ee4c72986693d0aa52f34cb03126137ab357f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a89562b8bc4cf9786b3c8f39b57da0413343b93")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
