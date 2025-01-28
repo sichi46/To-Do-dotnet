ee2ee4c72986693d0aa52f34cb03126137ab357f
sichi46 <willsonmulenga7@gmail.com>
2025-01-28 22:13:39 +0200

    Created all the  todo item routes

--- Program.cs
+++ Program.cs
@@ -27,4 +27,36 @@ app.MapPost("/todoitems", async(Todo todo, TodoDb db) =>{
     await db.SaveChangesAsync();
     return Results.Created($"/todoitems/{todo.Id}", todo);
 });
+
+
+app.MapPut("/todoitems/{id}", async(int id, Todo inputTodo, TodoDb db)=>{
+     
+     var todo = await db.Todos.FindAsync(id);
+     if (todo is null) return Results.NotFound();
+
+     todo.Name = inputTodo.Name;
+     todo.IsComplete = inputTodo.IsComplete;
+
+     await db.SaveChangesAsync();
+
+     return Results.NoContent();
+
+});
+
+app.MapDelete("/todoitems/{id}", async (int id, TodoDb db)=>
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+    else
+    {
+        return Results.NotFound();
+    }
+});
+
+
+app.Run();
     
