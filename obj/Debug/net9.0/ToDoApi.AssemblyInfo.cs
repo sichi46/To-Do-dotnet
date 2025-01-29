a1bfffefaa92992783f1d7c2d711128da1ce504c
sichi46 <willsonmulenga7@gmail.com>
2025-01-29 15:56:09 +0200

    Using typed results instead of ordinary results

--- obj/Debug/net9.0/ToDoApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/ToDoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd39b8b427ea1626e4d0762ccbe6d60e25fe899f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11f3189fcc82afbfc02069ceaf239fba161ec151")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
