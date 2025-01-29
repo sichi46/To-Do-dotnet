5f3b497fef267937328ab8c10c65f8ed4c1210f8
sichi46 <willsonmulenga7@gmail.com>
2025-01-29 16:05:07 +0200

    Implemented a Data Transer Object

--- Todo.cs
+++ Todo.cs
@@ -3,4 +3,5 @@ public class Todo{
     public string? Name{get;set;}
 
     public bool IsComplete{get;set;}
+    public string? Secret{get;set;}
 }
--- obj/Debug/net9.0/ToDoApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/ToDoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11f3189fcc82afbfc02069ceaf239fba161ec151")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1bfffefaa92992783f1d7c2d711128da1ce504c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
